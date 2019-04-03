090916ab38209adc68a5762bad3667d50192a477
Griffin Sink <Griffin.D.Sink@ou.edu>
2019-04-03 11:26:26 -0500

    class work

---
+++ ExampleWithMethods/ExampleWithMethods/Fruit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleWithMethods
+{
+    class Fruit
+    {
+        public string Name { get; set; }
+        public double Ounces { get; set; }
+        
+        
+        
+    }
+}
